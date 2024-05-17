caaca79abebdf953a2119e0d6439463cc1c705e4
Vishal Patel - personal <chiefvishpat@gmail.com>
2024-05-17 15:21:58 -0400

    completed this django demo project

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404, get_object_or_404, HttpResponseRedirect, reverse
+from .models import Question, Choice
 
 # Create your views here.
+
+#get questions and display them
+def index(request):
+    latest_questions = Question.objects.order_by('-pub_date')[:5]   #array of questions from new to old. Gets latest 5
+    context = {
+        'latest_questions': latest_questions
+    }
+    return render(request, 'polls/index.html', context)
+
+#show specific question and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question' : question} )
+
+#Get question and display results
+def results(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/results.html', {'question' : question})
+
+#vote for a question choice
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay the question voting form
+        return render(request, 'polls/detail.html', {
+            'question' : question,
+            'error_message' : "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing with POST data.
+        #This prevents data from being posted twice if a user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,))) #redirect to results page
